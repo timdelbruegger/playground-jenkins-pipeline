3807a3d7a786a7fcf53e4d0b29f7b47f04723872
Tim Delbrügger <tim.delbruegger@gmail.com>
2016-04-29 17:22:47 +0200

    reference to utility class must be a field

--- jenkins-workflows/src/components/ExampleA.groovy
+++ jenkins-workflows/src/components/ExampleA.groovy
@@ -1,11 +1,14 @@
 package components
 
+import groovy.transform.Field
+
 
 // ATTENTION
 // We have to load all used classes statically. Otherwise they will not be picked up during
 // "load" command in the bootstrap code. See Util.groovy
 // A description of the problem can be found at: http://bfischer.blogspot.de/2015/03/loading-workflow-scripts.html
 
+@Field
 def pipeline = load "jenkins-workflows/src/library/Pipeline.groovy"
 
 def start(){
