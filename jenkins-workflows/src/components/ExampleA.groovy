482a3e641ad215fadf961d15ba35b374a7e44803
Tim Delbrügger <tim.delbruegger@gmail.com>
2016-04-29 17:40:28 +0200

    Since it works without the second-level load, add some more checks

--- jenkins-workflows/src/components/ExampleA.groovy
+++ jenkins-workflows/src/components/ExampleA.groovy
@@ -1,22 +1,26 @@
 package components
 
-
+import groovy.transform.Field
 
 // ATTENTION
 // We have to load all used classes statically. Otherwise they will not be picked up during
 // "load" command in the bootstrap code. See Util.groovy
 // A description of the problem can be found at: http://bfischer.blogspot.de/2015/03/loading-workflow-scripts.html
 
-//@Field
-//def pipeline = load "jenkins-workflows/src/library/Pipeline.groovy"
-
 def start(){
 	echo "Started ExampleA workflow"
 	node{
 		echo "ExampleA reached first node"
+		
+		if(this.pipeline == null){
+			throw new IllegalStateException("Needed utility class is not present!")
+		}
 //		pipeline.start(env.BUILD_NAME)
 	}
 	echo "ExampleA workflow finished"
 }
+
+@Field
+def pipeline = load "jenkins-workflows/src/library/Pipeline.groovy"
 
 return this
