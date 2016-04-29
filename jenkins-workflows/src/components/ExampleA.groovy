392e4df6aa010699e848ab52af9494a5e6e09c0c
Tim Delbrügger <tim.delbruegger@gmail.com>
2016-04-29 17:03:23 +0200

    Do some output in the pipeline script

--- jenkins-workflows/src/components/ExampleA.groovy
+++ jenkins-workflows/src/components/ExampleA.groovy
@@ -1,6 +1,20 @@
 package components
 
+import library.Pipeline
 
-class ExampleA {
+// ATTENTION
+// We have to load all used classes statically. Otherwise they will not be picked up during
+// "load" command in the bootstrap code. See Util.groovy
+// A description of the problem can be found at: http://bfischer.blogspot.de/2015/03/loading-workflow-scripts.html
 
+def pipeline = new Pipeline(env.JOB_NAME)
+
+def start(){
+	echo "Started ExampleA workflow"
+	node{
+		echo "ExampleA reached first node"
+	}
+	echo "ExampleA workflow finished"
 }
+
+return this
